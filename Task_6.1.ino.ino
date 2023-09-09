71a1ddecdbc253b6f5769df54f5005c3a25bb879
amrelkona <143901085+amrelkona@users.noreply.github.com>
2023-09-09 16:58:42 +0300

    Update Task_6.1.ino.ino

--- Task_6.1.ino.ino
+++ Task_6.1.ino.ino
@@ -1,24 +1,32 @@
 #include <Wire.h>
 
-const int MPU6050_ADDRESS = 0x68; // I2C address of the MPU6050
+const int MPU6050_ADDRESS = 0x68;
+unsigned long previousTime = 0;
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);
-  // Configure DLPF to a recommended cutoff frequency of 42Hz
   Wire.beginTransmission(MPU6050_ADDRESS);
-  Wire.write(0x1A); 
-  Wire.write(0x03); 
+  Wire.write(0x1A);
+  Wire.write(0x03);
   Wire.endTransmission();
 }
 
 void loop() {
-  // Read the yaw angle from the sensor
-  int16_t rawYaw = readSensorValue(0x47); 
+  unsigned long currentTime = millis(); 
+    unsigned long elapsedTime = currentTime - previousTime;
+
+
+  previousTime = currentTime;
+
+  int16_t rawYaw = readSensorValue(0x47);
   float yawAngle = rawYaw / 131.0; 
-  // Print the yaw angle
+  float theta1 = yawAngle * (elapsedTime/1000.0);
+ float theta =+ theta1;
   Serial.print("Yaw angle: ");
   Serial.println(yawAngle);
+  Serial.println(elapsedTime);
+  Serial.println(theta);
 
   delay(100);
 }
@@ -32,4 +40,3 @@ int16_t readSensorValue(byte regAddress) {
   byte lowByte = Wire.read();
   return (highByte << 8) | lowByte;
 }
-
